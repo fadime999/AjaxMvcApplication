d8657dc6e1df31b784393809d2bf10499897d573
Fadime Kaplan <85846933+fadime999@users.noreply.github.com>
2025-03-14 23:51:59 +0300

    Add project files.

---
+++ AjaxMvcApp/Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using AjaxMvcApp.Data;
+using AjaxMvcApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AjaxMvcApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var products = _context.Products.ToList(); // ❌ Eğer _context null ise hata olur
+            return View(products);
+        }
+
+        [HttpGet]
+        public JsonResult SearchProducts(string searchTerm)
+        {
+            var productNames = _context.Products
+                .Where(p => p.Name.StartsWith(searchTerm)) // Arama işlemi
+                .ToList();
+
+            return Json(productNames);
+        }
+        // Ürün Ekleme İşlemi
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid) // Model doğrulaması
+            {
+                _context.Products.Add(product);
+                _context.SaveChanges();
+                return RedirectToAction("Index"); // Ürün eklenince listeye yönlendir
+            }
+
+            return View(product); // Hata olursa tekrar formu göster
+        }
+    }
+}
---
+++ AjaxMvcApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using AjaxMvcApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+
+namespace AjaxMvcApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
